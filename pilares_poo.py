2d752c33cf7b3fe0f77bbafbbfa0510bae70941c
Ana Livia <anaa.moraes.9@gmail.com>
2025-09-13 21:44:20 -0300

    Abstração

--- pilares_poo.py
+++ pilares_poo.py
@@ -30,7 +30,7 @@ animais = [dog, cat]
 for animal in animais:
   print(f"{animal.nome} faz: {animal.emitir_som()}")
 
-# Encapsulamento 
+# Encapsulamento: uso de atributos privados .__  
 print("Exemplo de encapsulamento:")
 class ContaBancaria:
   def __init__(self, saldo) -> None:
@@ -55,3 +55,35 @@ conta.depositar(valor=- 500)
 print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
 conta.sacar(valor= 200)
 print(f"Saldo da conta bancária: {conta.consultar_saldo()}")
+
+# se eu quiser criar uma outra conta bancaria? 
+''' conta_do_zezinho = ContaBancaria(saldo=50)'''
+
+# Abstração: criar molde para se construir classes, nos ajuda a proteger as caracteristicas.
+
+print("\nExemplo de abstração:")
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC): 
+
+  @abstractmethod
+  def ligar(self):
+    pass
+
+  @abstractmethod
+  def desligar(self):
+    pass
+
+class Carro(Veiculo):
+  def __init__(self) -> None: 
+    pass
+
+  def ligar(self):
+    return "Carro ligado usando a chave"
+  
+  def desligar(self):
+    return "Desligado usando a chave"
+  
+carro_amarelo = Carro()
+print(carro_amarelo.ligar())
+print(carro_amarelo.desligar())  
